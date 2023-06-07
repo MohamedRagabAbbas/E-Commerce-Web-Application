2e227002ddc3a1ed01795b84d07d97ecf518229c
MohamedRagabAbbas <Mohamed_Ragab02@aucegypt.edu>
2023-06-08 01:34:13 +0300

    Add connection string and swagger Api tester

--- E_CommerceWebApplication/E_CommerceWebApplication/Server/Program.cs
+++ E_CommerceWebApplication/E_CommerceWebApplication/Server/Program.cs
@@ -1,13 +1,24 @@
+using E_CommerceWebApplication.Server.Data;
 using Microsoft.AspNetCore.ResponseCompression;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
 
+// add Connection String
+builder.Services.AddDbContext<DBContextClass>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Add Swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
+app.UseSwaggerUI();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -20,7 +31,7 @@ else
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
+app.UseSwagger();
 app.UseHttpsRedirection();
 
 app.UseBlazorFrameworkFiles();
